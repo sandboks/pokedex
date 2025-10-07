c402aa5c1db6193c8e85012be35bc8a91b76b206
Sand <sandboxasdf@gmail.com>
2025-10-07 12:05:50 +1100

    REPL implemented, does nothing atm

--- src/repl.ts
+++ src/repl.ts
@@ -2,3 +2,34 @@ export function cleanInput(input: string): string[] {
     input = input.trim().toLowerCase();
     return input.split(/\s+/);
 }
+
+import { createInterface, type Interface } from "readline";
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Pokedex> ',
+});
+
+rl.on('line', (line: string) => {
+    let cleanedInput = cleanInput(line);
+    let input = cleanedInput[0];
+
+    switch (input) {
+        case '':
+            break;
+        case 'hello':
+            console.log("world");
+            break;
+        default:
+            console.log(`Command: '${input}'`);
+            break;
+    }
+    rl.prompt();
+}).on('close', () => {
+    console.log('Have a great day!');
+    process.exit(0);
+});
+
+export function startREPL() {
+    rl.prompt();
+}
